52af51aa768d4d36b18b020782e87dcd9d3958f0
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-07-11 16:59:18 +0530

    Max Element

---
+++ Arrays/MaxElement.java
@@ -0,0 +1,21 @@
+package Arrays;
+import java.util.*;
+public class MaxElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 1; i < size; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The maximum element in the array is: " + max);
+    }
+}
