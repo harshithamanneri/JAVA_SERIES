8d86bfe8d61fd34e02e61ae059b35573638448a6
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-07-14 21:51:17 +0530

    sum of an array

---
+++ Arrays/SumArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class SumArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        
+        int[] array = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        
+        System.out.println("The sum of the array elements is: " + sum);
+    }
+} 
