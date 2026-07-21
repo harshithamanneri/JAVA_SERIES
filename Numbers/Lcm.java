96938d8a416557d7112c1f9ff3961e5637fe0951
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-07-21 22:23:26 +0530

    LCM of two numbers

---
+++ Numbers/Lcm.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Lcm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        int lcm = (a * b) / gcd(a, b);
+        System.out.println("LCM of " + a + " and " + b + " is: " + lcm);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
