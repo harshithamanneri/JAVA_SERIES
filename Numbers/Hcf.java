0724057e4070d1586f645a3e3025158c9bcf7293
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-07-22 10:11:06 +0530

    HCF

---
+++ Numbers/Hcf.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Hcf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        int hcf = gcd(a, b);
+        System.out.println("HCF of " + a + " and " + b + " is: " + hcf);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
