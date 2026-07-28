c758d3e1b642dcd611534e365fbb32db8a132d22
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-07-28 09:54:53 +0530

    Calculator

---
+++ Numbers/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.print("Enter the count of multiples to print: ");
+        int count = sc.nextInt();
+        
+        System.out.println("The first " + count + " multiples of " + num + " are:");
+        for (int i = 1; i <= count; i++) {
+            System.out.println(num * i);
+        }
+    }
+}
