9662d4dad51f71510fcaea81f675a892154b43a9
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-07-30 09:47:44 +0530

    Duplicate String

---
+++ Strings/Duplicatestring.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class Duplicatestring {
+    public static void main(String[] args) {
+        String str = "programming";
+        int count;
+
+        char string[] = str.toCharArray();
+
+        System.out.println("Duplicate characters in a given string: ");
+        for (int i = 0; i < string.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < string.length; j++) {
+                if (string[i] == string[j] && string[i] != ' ') {
+                    count++;
+                    string[j] = '0';
+                }
+            }
+            if (count > 1 && string[i] != '0')
+                System.out.println(string[i]);
+        }
+    }
+    
+}
