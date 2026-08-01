96101f91e8ad9def93df1823f8dbcb5080cf89a9
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-08-01 10:11:02 +0530

    Rectangle star pattern

---
+++ Pattern/Rectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       for(int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
