51cf9f207c4883ba5598219176543c639dc7edff
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-08-05 10:24:40 +0530

    Square Pattern

---
+++ Pattern/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+    public static void main(String[] args) {
+        int size = 5; // You can change this size to generate a square of a different size
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
