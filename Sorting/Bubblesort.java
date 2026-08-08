1790f4c7299b722a8fd8e61d264c7a84612a86ae
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-08-08 22:51:43 +0530

    Bubble sort

---
+++ Sorting/Bubblesort.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+public class Bubblesort {
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Given Array");
+        for (int i : arr)
+            System.out.print(i + " ");
+        System.out.println();
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array");
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
+    
+}
