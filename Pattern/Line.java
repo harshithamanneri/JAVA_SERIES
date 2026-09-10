7401a12da80ff59ae08672332366aa1a602162e6
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-09-10 21:49:02 +0530

    Line pattern length

---
+++ Pattern/Line.java
@@ -0,0 +1,12 @@
+public class Line {
+    public static void main(String[] args) {
+        int length = 10;
+
+        System.out.println("Line of length " + length + ":");
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+    
+}
